917fb35639c94d919578795d4ecee7b8aa35875d
jason1213 <jason1213@gmail.com>
2016-09-23 07:52:42 +0800

    add codes

---
+++ config.js
@@ -0,0 +1,17 @@
+var services = {
+  name: 'Agriculture-Mobile',
+  host: 'http://weixin.boybai.cn',
+  port: 6000
+};
+var db = {
+  host: '',
+  name: '',
+  user: '',
+  pass: ''
+}
+var configs = {
+  services: services,
+  db: db
+};
+
+module.exports = configs;
