d345e9521ef9099ddb7063fb2033104235cae9af
jason1213 <jason1213@gmail.com>
2016-09-24 12:25:52 +0800

    init project codes

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 var express = require('express');
+var configs = require('./config');
+
+
+var app = express();
+
+
+var port = process.env.PORT || configs.services.PORT;
 
 
 
+
+app.listen(port, function () {
+  console.log('Agriculture Mobile Service API running at ' + port + ' port!');
+});
